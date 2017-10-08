ec1cc66dc534ac3bdbc968a2b76f1a921f206040
Stoycho Petrov <scyberboy@gmail.com>
2017-10-08 18:53:48 +0300

    extend with blah :)

--- StoychoPetrovHomework03/src/scyberboy/homework/classes/ProblemsDateTime.java
+++ StoychoPetrovHomework03/src/scyberboy/homework/classes/ProblemsDateTime.java
@@ -5,6 +5,8 @@ public class ProblemsDateTime {
 	public static void main(String[] args) {
 		
 		System.out.println("The current time is: " + MyCurrDate.getCurrDateTime());
+		
+		System.out.println("Proba: " + MyCurrDate.getCurrDateTime().toString());
 
 	}
 
