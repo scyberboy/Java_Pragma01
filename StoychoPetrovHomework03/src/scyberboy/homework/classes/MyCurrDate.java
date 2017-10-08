dbc9159269cf33d85f56abbf327c292efb13a09e
Stoycho Petrov <scyberboy@gmail.com>
2017-10-08 18:42:06 +0300

    Part two - date & time class

---
+++ StoychoPetrovHomework03/src/scyberboy/homework/classes/MyCurrDate.java
@@ -0,0 +1,14 @@
+package scyberboy.homework.classes;
+
+import java.time.LocalDateTime;
+
+public class MyCurrDate {
+	
+	public static LocalDateTime getCurrDateTime() {
+		
+		return LocalDateTime.now();
+		
+	}
+	
+
+}
---
+++ StoychoPetrovHomework03/src/scyberboy/homework/classes/ProblemsDateTime.java
@@ -0,0 +1,11 @@
+package scyberboy.homework.classes;
+
+public class ProblemsDateTime {
+
+	public static void main(String[] args) {
+		
+		System.out.println("The current time is: " + MyCurrDate.getCurrDateTime());
+
+	}
+
+}
